2bab9eb41882a0abe477ed44a389eb794e39ced3
Sean Seung Won Lee <swdev1202@gmail.com>
2019-11-30 08:25:30 -0800

    train script

---
+++ train.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from tgan.model import TGANModel
+
+data = pd.read_csv('examples/data/hcv.csv')
+
+continuous_columns = [0,2,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
+
+tgan = TGANModel(
+    continuous_columns,
+    output='output',
+    gpu=None,
+    max_epoch=100,
+    steps_per_epoch=10000,
+    save_checkpoints=True,
+    restore_session=True,
+    batch_size=200,
+    z_dim=200,
+    noise=0.2,
+    l2norm=0.00001,
+    learning_rate=0.001,
+    num_gen_rnn=100,
+    num_gen_feature=100,
+    num_dis_layers=1,
+    num_dis_hidden=100,
+    optimizer='AdamOptimizer'
+)
+
+tgan.fit(data)
+model_path = 'examples/demo/my_model'
+tgan.save(model_path)
+print("train finished")
